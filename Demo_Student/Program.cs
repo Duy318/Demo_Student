53904fefb33cba7b8eb2b4af1ca267c263831bd7
ADMIN <ADMIN@DESKTOP-RI9254C>
2023-01-09 15:41:14 +0700

    Add project files.

---
+++ Demo_Student/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo_Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Name = "Duy";
+            student1.Age = 30;
+            student1.Gpa = 2.3;
+            student1.PrintStudent();
+
+            var student2 = new Student("Tan", 40, 2.8);
+            Console.WriteLine(student2.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo_Student/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Student
+{
+    internal class Student:Object
+    {
+        public string? Name { get; set; }
+        public int Age { get => age; set => age = value; }
+        private int age;
+        private double gpa;
+        public double Gpa { set { if (value >= 0 && value <= 4) gpa = value; }
+                get{ return gpa; }
+        }
+        //contructor
+        public Student(string name, int age, double gpa)
+        {
+            Name = name;
+            this.Age = age;
+            this.gpa = gpa;
+        }
+
+        public Student()
+        {
+        }
+
+        public void PrintStudent()
+        {
+            Console.WriteLine($"Student has Name={Name}, Age = {age}, GPA={gpa}");
+        }
+        public override string ToString()
+        {
+            return ($"Student has Name={Name}, Age = {age}, GPA={gpa}");
+        }
+    }
+}
